20fda3b5b5b18f9401bb5b3d39290f71efdc1776
Lokenath Karmakar <ilokenathkarmakar@gmail.com>
2021-11-04 13:28:18 +0000

    complete json clean task

---
+++ technicalAssesment/nodeJsonCleaning.js
@@ -0,0 +1,30 @@
+const https = require("https");
+https.get("https://coderbyte.com/api/challenges/json/json-cleaning", (resp) => {
+  var data = "";
+  resp.on("data", (chunk) => {
+    data += chunk;
+  });
+
+  resp.on("end", () => {
+    if (resp.statusCode === 200) {
+      data = JSON.parse(data);
+    }
+    console.log(cleanObj(data));
+  });
+});
+
+const cleanObj = (obj) => {
+  const valuesToDiscard = ["", "N/A", "-"];
+
+  if (typeof obj !== "object") return obj;
+
+  const newObj = Object.fromEntries(
+    Object.entries(obj)
+      .map(([key, value]) => [key, cleanObj(value)])
+      .filter(([key, value]) => {
+        return !valuesToDiscard.includes(value);
+      })
+  );
+
+  return Array.isArray(obj) ? Object.values(newObj) : newObj;
+};
